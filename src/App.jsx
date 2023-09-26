5479ca21a6655fd88741d57ba85cfe2a9e4ddc4d
Alex HM <alex13pks@gmail.com>
2023-09-25 19:57:41 -0600

    Added Tailwind sort plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   return (
     <div>
-      <h1 className="text-8xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 to-pink-400">
+      <h1 className="bg-gradient-to-r from-indigo-400 to-pink-400 bg-clip-text text-8xl font-extrabold text-transparent">
         We love tailwind 💙
       </h1>
+      <p className="p-4 pl-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      </p>
     </div>
   );
 }
